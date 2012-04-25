6b69375347653223534558c0f45a45383c6168e2
Wei Li <oldrev@gmail.com>
2012-04-25 23:41:04 +0800

    初步的记账凭证格式改善

--- src/addons/l10n_cn_account_report/report/account_move_parser.py
+++ src/addons/l10n_cn_account_report/report/account_move_parser.py
@@ -2,13 +2,18 @@
 
 from report import report_sxw
 from report.report_sxw import rml_parse
+import util
 
-class AccountMoveParser(report_sxw.rml_parse):
+class Parser(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
-        super(AccountMoveParser, self).__init__(cr, uid, name, context)
+        super(Parser, self).__init__(cr, uid, name, context)
         self.localcontext.update({
-            'hello_world':self.hello_world,
+            'rmb_upper': util.rmb_upper,
+            'hello_world': self.hello_world,
         })
+
+    def set_context(self, objects, data, ids, report_type=None):
+        return super(Parser, self).set_context(objects, data, ids, report_type=report_type)
 
     def hello_world(self, name):
         return "Hello, %s!" % name
